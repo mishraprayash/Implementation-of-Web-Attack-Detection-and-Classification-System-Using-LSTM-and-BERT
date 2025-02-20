7f8e323a1436e7e2e324317a9d93a0f03f878315
mishraprayash <mishraprayash11@gmail.com>
2025-02-20 17:24:40 +0545

    updated db_url

--- app/config.py
+++ app/config.py
@@ -1,3 +1,8 @@
-DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/Logging"  # Replace with your actual database URL, and use the respective library for handling session and db connection
+# Replace with your actual database URL, and use the respective library for handling session and db connection
+
+# DATABASE_URL = "mysql+pymysql://<username>:<password>@host:port/<db_name>"
+
+DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/Logging_V2"  
+
 MODEL_PATH = "./lstm_model/model.pt"
 VOCAB_PATH = "./lstm_model/vocab.pth"
